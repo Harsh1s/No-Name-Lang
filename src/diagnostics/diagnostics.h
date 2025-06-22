d20b99e35938a9a20798ca639ab3dc8b0e7a8d24
Harsh1s <harsh.raj.cd.ece21@itbhu.ac.in>
2025-06-22 13:01:05 +0530

    Add identifier start classifier

--- src/diagnostics/diagnostics.h
+++ src/diagnostics/diagnostics.h
@@ -3,3 +3,5 @@
 #include <stddef.h>
 
 typedef struct { size_t line; size_t column; const char* message; } nnl_diagnostic;
+int nnl_is_identifier_start(char ch);
+int nnl_is_identifier_continue(char ch);
