2e3923afacbacc9b724dc1c3a1f31c6af48c7680
Harsh1s <harsh.raj.cd.ece21@itbhu.ac.in>
2025-06-27 19:20:53 +0530

    Add identifier continuation classifier

--- src/diagnostics/diagnostics.h
+++ src/diagnostics/diagnostics.h
@@ -5,3 +5,4 @@
 typedef struct { size_t line; size_t column; const char* message; } nnl_diagnostic;
 int nnl_is_identifier_start(char ch);
 int nnl_is_identifier_continue(char ch);
+size_t nnl_count_lines(const char* source);
