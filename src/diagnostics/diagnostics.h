7458f43000cab673086a984ecdcb568507e3f27d
Harsh1s <harsh.raj.cd.ece21@itbhu.ac.in>
2025-07-02 10:45:24 +0530

    Add source line counting helper

--- src/diagnostics/diagnostics.h
+++ src/diagnostics/diagnostics.h
@@ -6,3 +6,4 @@ typedef struct { size_t line; size_t column; const char* message; } nnl_diagnost
 int nnl_is_identifier_start(char ch);
 int nnl_is_identifier_continue(char ch);
 size_t nnl_count_lines(const char* source);
+nnl_diagnostic nnl_make_diagnostic(size_t line, size_t column, const char* message);
